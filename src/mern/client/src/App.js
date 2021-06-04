cb005abb2f0d6c8d2464d8ce6be33f45c1690903
megleemeg <65801522+megleemeg@users.noreply.github.com>
2021-06-04 23:55:33 +0800

    Mern starter code added (#1)
    * added server side
    
    * added client side
    
    * Delete tryout-style.css
    
    Experimental file. Not require for starter code
    
    * Update and rename tryout.js to example.js
    
    Experimental file.
    
    Co-authored-by: megleemeg <megch.lee@utoronto.ca>

---
+++ src/mern/client/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// We use Route in order to define the different routes of our application
+import { Route } from "react-router-dom";
+const App = () => {
+    return null;
+};
+
+export default App;
---
+++ src/mern/client/src/components/example.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom"; //for the web to be responsive
+
+class example extends React.Component{
+    render()
+}
