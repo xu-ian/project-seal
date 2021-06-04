0da159d2c11f93a9f99c86f81c0c5750d18b0c2a
megleemeg <65801522+megleemeg@users.noreply.github.com>
2021-06-04 19:54:46 +0800

    Update and rename tryout.js to example.js
    Experimental file.

--- src/mern/client/src/components/example.js
+++ src/mern/client/src/components/example.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom"; //for the web to be responsive
-import './tryout-style.css';
-class Tryout extends React.Component{
+
+class example extends React.Component{
     render()
 }
