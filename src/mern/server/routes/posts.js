2b732d3b4245731696626e3dd55c6284d8b777fc
Arianne Lavada <ariannelavada@gmail.com>
2021-06-08 19:06:10 -0400

    SEAL 4: Initial commit, added schemas for Posts and Comments, implemented API for Posts, modified server.js for testing

---
+++ src/mern/server/routes/posts.js
@@ -0,0 +1,76 @@
+const { ObjectID } = require('bson');
+const { json } = require('express');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const Post = require('../models/Post'); 
+const postsRoutes = express.Router();
+
+// Get a list of all the posts.
+postsRoutes.route('/').get((req, res) => {
+    Post.find()
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(err => {
+            res.status(400).json({ msg: err.msg });
+        });
+});
+
+// Get a specific post by ID.
+postsRoutes.route('/:id').get((req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            res.json(post);
+        })
+        .catch(err => {
+            res.status(400).json({ msg: err.msg});
+        });
+});
+
+// Create a post.
+postsRoutes.route("/add").post((req, res) => {
+    let newPost = new Post({
+        author: req.body.author,
+        title: req.body.title,
+        content: req.body.content,
+        tags: req.body.tags
+    });
+
+    newPost.save()
+        .then(post => {
+            res.status(201).json(post);
+        })
+        .catch(err => {
+            res.status(400).json({ msg: err.msg });
+        });
+});
+
+// Update a specific post by ID.
+postsRoutes.route("/update/:id").patch((req, res) => {
+    Post.updateOne({ _id: ObjectID(req.params.id) }, {
+        $set: {
+            content: req.body.content,
+            tags: req.body.tags
+        }
+    })
+        .then(() => {
+            res.json({ msg : "Post has been updated." });
+        })
+        .catch(err => {
+            res.status(400).json({ msg: err.msg });
+        });
+});
+
+// Deleting a specific post by ID.
+postsRoutes.route("/delete/:id").delete((req, res) => {
+    Post.deleteOne({ _id: ObjectID(req.params.id) })
+        .then(() => {
+            res.json({ msg: "Post has been deleted." });
+        })
+        .catch(err => {
+            res.status(400).json({ msg: err.msg });
+        });
+});
+
+module.exports = postsRoutes;
--- src/mern/server/server.js
+++ src/mern/server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config({ path: "./config.env" });
 const port = 8080;
 const MongoClient = require('mongodb').MongoClient;
@@ -14,8 +15,25 @@ client.connect(err => {
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
+
+// Importing routes for posts
+const postsRouter = require('./routes/posts');
+app.use('/posts', postsRouter);
+
+const commentsRouter = require('./routes/comments');
+app.use('/comments', commentsRouter);
 // get driver connection
 const dbo = require("./db/conn");
+const postsRoutes = require("./routes/posts");
+const commentsRoutes = require("./routes/comments");
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.post("/post", (req, res) => {
   console.log("Connected to React");
@@ -24,9 +42,6 @@ app.post("/post", (req, res) => {
 
 app.listen(port, () => {
   // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-
-  });
+  
   console.log(`Server is running on port: ${port}`);
 });
