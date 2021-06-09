ddf2abd29747fc150453ba18ff42e1507c673520
megleemeg <megch.lee@utoronto.ca>
2021-06-09 18:51:50 +0800

    SEAL-3: Initialize Frontend and Completed Backend

--- src/mern/client/src/App.js
+++ src/mern/client/src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
-
 // We use Route in order to define the different routes of our application
 import { Route } from "react-router-dom";
-const App = () => {
-    return null;
-};
 
-export default App;
+//component for the part
+import CompanyProfileList from "./components/companyProfile/companyProfile";
+import CreateCompanyProfile from "./components/companyProfile/createCompanyProfile";
+import EditCompanyProfile from "./components/companyProfile/editCompanyProfile";
+ 
+
+const App = () => {
+    return (
+        //anything here needs to be a single element (aka a <div>)
+        <div>
+            <Route exact path="/">
+                Main Page
+            </Route>
+            <Route path="/company-profile/"> 
+                <CompanyProfileList />
+            </Route>
+            <Route path="/company-profile/create">
+                <CreateCompanyProfile />
+            </Route>
+            <Route path="/edit/:id" component={EditCompanyProfile} />
+        </div>
+    )
+}
+
+export default App
