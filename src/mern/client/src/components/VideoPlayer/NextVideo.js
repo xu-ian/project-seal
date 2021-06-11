987e3ec583225ce2756f49c61a551ea7d33d7c32
PengJie-Bob <84425588+PengJie-Bob@users.noreply.github.com>
2021-06-11 02:43:05 -0400

    SEAL-7: Video-Updated Styling
    -Updated css file for VideoPlayer
    -Added next video tab
    -Added empty documentation.md (will be updated soon)

---
+++ src/mern/client/src/components/VideoPlayer/NextVideo.js
@@ -0,0 +1,9 @@
+const NextVideo = () => {
+	return (
+		<div>
+			Next Up...
+		</div>
+	)
+}
+
+export default NextVideo
--- src/mern/client/src/components/VideoPlayer/index.js
+++ src/mern/client/src/components/VideoPlayer/index.js
@@ -2,18 +2,20 @@ import Title from './Title'
 import Video from './Video'
 import Header from './Header'
 import Description from './Description'
+import NextVideo from './NextVideo'
 import './index.css'
 function App() {
   return (
     <div className='container'>
-			<div className='video'>
-				<Header/>
+			<Header/>
+			<aside id='video-sidebar'>
+				<NextVideo/>
+			</aside>
+			<section id='video-main'>
       	<Video/>
       	<Title/>
 				<Description/>
-			</div>
-			<aside className='sidebar'>
-			</aside>
+			</section>
     </div>
   );
 }
