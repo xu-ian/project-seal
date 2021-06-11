0a3efa298ca0cd89f4ef5dc6e391ca8c4a7b1c46
J <yuhengwu.wu@mail.utoronto.ca>
2021-06-12 03:44:00 +0800

    SEAL-1 & SEAL-2: Changed buttons style in select.js

--- src/mern/client/src/components/select.js
+++ src/mern/client/src/components/select.js
@@ -4,7 +4,7 @@ import './styling.css';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
-import { withStyles } from '@material-ui/core/styles';
+// import { withStyles } from '@material-ui/core/styles';
 import 'fontsource-roboto';
 import Typography from '@material-ui/core/Typography';
  
@@ -13,22 +13,22 @@ import Typography from '@material-ui/core/Typography';
 const Select = () => {
 
   // button styling
-  const ButtonStyle = withStyles({
-    root: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      borderRadius: 5,
-      border: 0,
-      color: 'white',
-      height: 100,
-      fontSize: 30,
-      width: 300,
-      padding: '0px 30px',
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    },
-    label: {
-      textTransform: 'capitalize',
-    },
-  })(Button);
+  // const ButtonStyle = withStyles({
+  //   root: {
+  //     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  //     borderRadius: 5,
+  //     border: 0,
+  //     color: 'white',
+  //     height: 100,
+  //     fontSize: 30,
+  //     width: 300,
+  //     padding: '0px 30px',
+  //     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  //   },
+  //   label: {
+  //     textTransform: 'capitalize',
+  //   },
+  // })(Button);
 
   let history = useHistory();
 
@@ -50,8 +50,12 @@ const Select = () => {
 
       {/* Two buttons */}
       <ButtonGroup>
-        <ButtonStyle onClick={toLogin}>Login</ButtonStyle>
-        <ButtonStyle onClick={toSignup}>Sign up</ButtonStyle>
+        <Button variant="contained" size="large" color="primary" onClick={toLogin}>
+          Sign In
+        </Button>
+        <Button variant="contained" size="large" color="secondary" onClick={toSignup}>
+          Sign Up
+        </Button>        
       </ButtonGroup>
     </div>
   );
