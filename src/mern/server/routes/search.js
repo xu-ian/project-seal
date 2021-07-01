dce6cc0d9aea80b8082177fd3ddb96241206b9d7
J <yuhengwu.wu@mail.utoronto.ca>
2021-07-02 00:31:43 +0800

    SEAL-16: Back-end completed

--- src/mern/server/routes/search.js
+++ src/mern/server/routes/search.js
@@ -4,10 +4,22 @@ const searchRoutes = express.Router();
 const mongoose = require("mongoose");
 const Company = require('../models/Company');
 
-// Route for Initial state
+// Route for Initial state for company search
 searchRoutes.route('/listcompanies').get((req, res) => {
   // Get 15 companies information 
   Company.find({}, {company_title:1, tagline:1, logo:1}, {limit: 15}).then(companies => {
+    res.json(companies);
+
+  }).catch(err => {
+    res.status(400).json({ msg: err.msg });
+  })
+});
+
+// For search input by name
+searchRoutes.route('/:name').get((req, res) => {
+  let name = new RegExp(req.params.name, 'i');
+  // Get maximum 15 companies which company_title contains "name" as substring.
+  Company.find({company_title : name}, {company_title:1, tagline:1, logo:1}, {limit: 15}).then(companies => {
     res.json(companies);
 
   }).catch(err => {
