939fd7f74b6d8f8e1da7fdef7d9da29deae7ac75
megleemeg <65801522+megleemeg@users.noreply.github.com>
2021-07-02 15:25:20 +0800

    SEAL-16: Update search.js

--- src/mern/server/routes/search.js
+++ src/mern/server/routes/search.js
@@ -7,13 +7,16 @@ const Company = require('../models/Company');
 // Route for Initial state for company search
 searchRoutes.route('/listcompanies').get((req, res) => {
   // Get 15 companies information 
-  Company.find({}, {company_title:1, tagline:1, logo:1}, {limit: 15}).then(companies => {
+  Company.find({}, {company_title:1, tagline:1, logo:1}).then(companies => {
     res.json(companies);
 
   }).catch(err => {
     res.status(400).json({ msg: err.msg });
   })
 });
+
+//  Company.find({}, {company_title:1, tagline:1, logo:1}, {limit: 15}).then(companies => {
+
 
 // For search input by name
 searchRoutes.route('/:name').get((req, res) => {
