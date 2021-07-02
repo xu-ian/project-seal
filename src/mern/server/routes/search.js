9dc20ec4f75fee001c1984553e5edf7bad1bace5
liangyon <yonlin.liang@mail.utoronto.ca>
2021-07-02 01:29:08 -0700

    SEAL19 added my backend components to search.js routes

--- src/mern/server/routes/search.js
+++ src/mern/server/routes/search.js
@@ -3,6 +3,7 @@ const searchRoutes = express.Router();
 
 const mongoose = require("mongoose");
 const Company = require('../models/Company');
+const User = require('../models/user');
 
 // Route for Initial state for company search
 searchRoutes.route('/listcompanies').get((req, res) => {
@@ -18,8 +19,8 @@ searchRoutes.route('/listcompanies').get((req, res) => {
 //  Company.find({}, {company_title:1, tagline:1, logo:1}, {limit: 15}).then(companies => {
 
 
-// For search input by name
-searchRoutes.route('/:name').get((req, res) => {
+// For search company input by name
+searchRoutes.route('/companies/:name').get((req, res) => {
   let name = new RegExp(req.params.name, 'i');
   // Get maximum 15 companies which company_title contains "name" as substring.
   Company.find({company_title : name}, {company_title:1, tagline:1, logo:1}, {limit: 15}).then(companies => {
@@ -30,5 +31,29 @@ searchRoutes.route('/:name').get((req, res) => {
   })
 });
 
+
+//route to get list of users
+searchRoutes.route('/listusers').get((req, res) => {
+  // Get 15 companies information 
+  User.find({}, {username:1, userbio:1, email:1}).then(users => {
+    res.json(users);
+
+  }).catch(err => {
+    res.status(400).json({ msg: err.msg });
+  })
+});
+
+
+// For search company input by name
+searchRoutes.route('/users/:name').get((req, res) => {
+  let name = new RegExp(req.params.name, 'i');
+  // Get maximum 15 companies which company_title contains "name" as substring.
+  User.find({username : name}, {username:1, userbio:1, email1}, {limit: 15}).then(users => {
+    res.json(users);
+
+  }).catch(err => {
+    res.status(400).json({ msg: err.msg });
+  })
+});
 
 module.exports = searchRoutes;
