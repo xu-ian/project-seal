367f16a9d5060ef4e2441a26df7036354f442297
PengJie-Bob <84425588+PengJie-Bob@users.noreply.github.com>
2021-07-16 13:07:43 -0400

    SEAL-13: Time formatting
    -Formatted time to have 2 digits always
    -May want to change to 12h based time

--- src/mern/client/src/components/Calendar/Event.js
+++ src/mern/client/src/components/Calendar/Event.js
@@ -1,7 +1,7 @@
 const Event = ({ content }) => {
 	return (
 		<div className='event-item'>
-			<p>{new Date(content.date).getHours() + ':' + new Date(content.date).getMinutes()}</p>
+			<p>{(new Date(content.date).getHours()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false}) + ':' + (new Date(content.date).getMinutes()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})}</p>			
 			<p>{content.name}</p>
 		</div>
 	)
