3a72d80b568f25fbb84c3082c25d9d8274b19db1
Arianne Lavada <ariannelavada@gmail.com>
2021-07-22 16:39:56 -0400

    SEAL-6: Added Video schema

---
+++ src/mern/server/models/Video.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const VideoSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    course: {
+        type: String,
+        required: true
+    },
+    path: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Video', VideoSchema);
